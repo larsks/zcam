cc2bc77793ad8ced40ab060c0b89d5f141afca83
Lars Kellogg-Stedman <lars@redhat.com>
2017-09-27 17:36:37 -0400

    add --json to sendmessage

--- zcam/sendmessage.py
+++ zcam/sendmessage.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import json
 
 import zcam.app.base
 import zcam.app.zmq
@@ -10,13 +11,20 @@ LOG = logging.getLogger(__name__)
 class SendMessage(zcam.app.zmq.ZmqClientApp):
     def create_parser(self):
         p = super().create_parser()
+        p.add_argument('--json',
+                       action='store_true')
         p.add_argument('topic')
-        p.add_argument('kv', nargs='*',
-                       type=zcam.app.base.KeyValueArgument)
+        p.add_argument('msg', nargs='*')
         return p
 
     def main(self):
-        msg = {k: v for k, v in self.args.kv}
+        if self.args.json:
+            msg = json.loads(self.args.msg[0])
+        else:
+            msg = {k: v
+                   for k, v in [kv.split('=')
+                                for kv in self.args.msg.split(' ')]}
+
         LOG.info('sending message %s %s', self.args.topic, msg)
         self.send_message(self.args.topic, **msg)
 
