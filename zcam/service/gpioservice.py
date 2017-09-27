1c7a93ac6871af8b666e03c08585ae2a10802411
Lars Kellogg-Stedman <lars@redhat.com>
2017-09-27 09:49:30 -0400

    teach gpioservice about --pin

--- zcam/service/gpioservice.py
+++ zcam/service/gpioservice.py
@@ -1,5 +1,4 @@
 import logging
-import zmq
 
 from RPi import GPIO
 
@@ -10,9 +9,19 @@ LOG = logging.getLogger(__name__)
 
 class GpioServiceApp(zcam.app.ZmqClientApp):
     def __init__(self, sensorname, bouncetime=None, **kwargs):
-        super().__init__(**kwargs)
         self.sensorname = sensorname
         self.bouncetime = bouncetime
+        super().__init__(**kwargs)
+
+    def create_parser(self):
+        p = super().create_parser()
+        p.add_argument('--pin')
+        return p
+
+    def create_overrides(self):
+        return super().create_overrides() + [
+            ('pin', self.name, '{}_pin'.format(self.sensorname)),
+        ]
 
     def main(self):
         pin = self.config.get(self.name, '{}_pin' % self.sensorname)
