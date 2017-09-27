cb40dbfae9e8005097aebab64e737c8d8443422f
Lars Kellogg-Stedman <lars@redhat.com>
2017-09-27 15:27:25 -0400

    update dht service

--- zcam/service/dht.py
+++ zcam/service/dht.py
@@ -2,60 +2,55 @@ import Adafruit_DHT
 import logging
 import time
 
-import zcam.app
+import zcam.app.zmq
 
 LOG = logging.getLogger(__name__)
 
+default_interval = 30
+default_model = 'dht22'
 
-class DHTSensorApp(zcam.app.ZmqClientApp):
+
+class DHTSensorApp(zcam.app.zmq.ZmqClientApp):
+    namespace = 'zcam.sensor.dht'
 
     def create_parser(self):
         p = super().create_parser()
         p.add_argument('--pin')
         p.add_argument('--model')
-        p.add_argument('--interval', type=int)
-        p.add_argument('name')
+        p.add_argument('--interval')
         return p
 
     def create_overrides(self):
         return super().create_overrides() + [
-            ('pin', self.name, '{}_pin'.format(self.args.name)),
-            ('model', self.name, '{}_model'.format(self.args.name)),
-            ('interval', self.name, '{}_interval'.format(self.args.name)),
+            'pin', 'model', 'interval'
         ]
 
     def main(self):
-        pin = self.config.getint(self.name,
-                                 '{}_pin'.format(self.args.name))
-        model = self.config.getint(self.name,
-                                   '{}_model'.format(self.args.name),
-                                   fallback='dht22')
-        interval = self.config.getint(self.name,
-                                      '{}_interval'.format(self.args.name),
-                                      fallback=60)
+        pin = int(self.get('pin'))
+        model = self.get('model', default_model)
+        interval = int(self.get('interval', default_interval))
 
         model_id = getattr(Adafruit_DHT, model.upper())
 
         LOG.info('starting dht sensor %s on pin %d',
-                 self.args.name, pin)
+                 self.name, pin)
 
         while True:
-            humidity, temperature = Adafruit_DHT.read_retry(model_id, pin)
-            if humidity is not None and temperature is not None:
+            reading = Adafruit_DHT.read_retry(model_id, pin)
+            if all(val is not None for val in reading):
                 LOG.debug('dht sensor %s on pin %d '
                           'got humidity = %f, temp = %f',
-                          self.args.name, pin, humidity, temperature)
-                self.send_message(
-                    'sensor.dht.{}.temperature'.format(self.args.name),
-                    tags=dict(pin=pin, model=model),
-                    fields=dict(value=temperature))
-                self.send_message(
-                    'sensor.dht.{}.humidity'.format(self.args.name),
-                    tags=dict(pin=pin, model=model),
-                    fields=dict(value=humidity))
+                          self.name, pin, *reading)
+
+                for i, label in enumerate(['temperature', 'humidity']):
+                    self.send_message(
+                        '{}.{}'.format(self.name, label),
+                        tags=dict(pin=pin, instance=self.args.instance,
+                                  model=model),
+                        fields=dict(value=reading[i]))
             else:
                 LOG.warning('dht sensor %s failed to read value from pin %d',
-                            self.args.name, pin)
+                            self.name, pin)
 
             time.sleep(interval)
 
