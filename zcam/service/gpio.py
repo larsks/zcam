0f857176c0298b6a44098b262fb47aa5a01c4d91
Lars Kellogg-Stedman <lars@redhat.com>
2017-09-27 21:40:29 -0400

    teach gpioservice about --pull-up and --pull-down

--- zcam/service/gpio.py
+++ zcam/service/gpio.py
@@ -14,16 +14,34 @@ class GpioService(zcam.app.zmq.ZmqClientApp):
         p = super().create_parser()
         p.add_argument('--pin')
         p.add_argument('--bouncetime')
+        g = p.add_mutually_exclusive_group()
+        g.add_argument('--pull-up',
+                       action='store_true')
+        g.add_argument('--pull-down',
+                       action='store_true')
         return p
 
     def create_overrides(self):
         return super().create_overrides() + [
             'pin', 'bouncetime',
+            'pull_up', 'pull_down'
         ]
 
     def main(self):
         pin = int(self.get('pin'))
         bouncetime = self.get('bouncetime', None)
+        pull_up = self.get('pull_up', 'false').lower() == 'true'
+        pull_down = self.get('pull_down', 'false').lower() == 'true'
+
+        if pull_up and pull_down:
+            raise ValueError('pull_up and pull_down are mutually exclusive')
+
+        if pull_up:
+            pud = GPIO.PUD_UP
+        elif pull_down:
+            pud = GPIO.PUD_DOWN
+        else:
+            pud = GPIO.PUD_OFF
 
         waitargs = {}
         if bouncetime:
@@ -31,7 +49,7 @@ class GpioService(zcam.app.zmq.ZmqClientApp):
 
         LOG.info('starting gpio sensor %s on pin %d', self.name, pin)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.IN)
+        GPIO.setup(pin, GPIO.IN, pull_up_down=pud)
 
         while True:
             GPIO.wait_for_edge(pin, GPIO.BOTH, **waitargs)
